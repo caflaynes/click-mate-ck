677fa6db0dd8a68e57158034fa8363c136df8ea7
Carlo-Laynes <132353416+Carlo-Laynes@users.noreply.github.com>
2024-11-17 22:57:33 +0800

    enabled v7transition for router react-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom';
-import './styles/index.scss'
-import App from './App.tsx'
+// src/main.tsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles/index.scss';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
+    <Router
+      future={{
+        v7_startTransition: true, // Enable v7_startTransition
+      }}
+    >
       <App />
     </Router>
-  </StrictMode>,
-)
+  </StrictMode>
+);
