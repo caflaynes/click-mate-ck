2744a65fb81e8014cb971f82b0a5d8ca14509c22
Carlo-Laynes <132353416+Carlo-Laynes@users.noreply.github.com>
2024-11-17 20:55:38 +0800

    updated font path in partial

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/fonts/*', // Adjust this to the source folder where your fonts are
+          dest: 'assets/fonts' // This is where the fonts will go in the build folder
+        }
+      ]
+    })
+  ],
   base: '/click-mate-ck/', 
   css: {
     preprocessorOptions: {
@@ -15,4 +26,4 @@ export default defineConfig({
   build: {
     outDir: 'build', // This sets the output directory to "build"
   }
-})
+});
