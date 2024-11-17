a691ea9942fd29a4d310b9bce6ca0e315d7e0717
Carlo-Laynes <132353416+Carlo-Laynes@users.noreply.github.com>
2024-11-18 00:13:24 +0800

    not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
 // src/App.tsx
 import React from 'react';
 import './App.scss';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Slogan from './components/Layouts/Slogan/Slogan';
 import Navbar from './components/Layouts/Navbar/Navbar';
+
+import HomePage from './pages/Home/HomePage';
+import DigitalCamerasPage from './pages/Digital Cameras/DigitalCamerasPage';
+import HowItWorksPage from './pages/How It Works/HowItWorksPage';
+import PricingPage from './pages/Pricing/PricingPage';
+import ContactUsPage from './pages/Contact Us/ContactUsPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <div>404 Not Found</div>
+  },
+  {
+    path: '/digital-cameras',
+    element: <DigitalCamerasPage />
+  },
+  {
+    path: '/how-it-works',
+    element: <HowItWorksPage />
+  },
+  {
+    path: '/pricing',
+    element: <PricingPage />
+  },
+  {
+    path: '/contact-us',
+    element: <ContactUsPage />
+  }
+]);
 
 const App: React.FC = () => {
   return (
@@ -12,6 +43,7 @@ const App: React.FC = () => {
       </header>
       <Navbar logoSrc="./assets/images/logo.svg"/>
       <main>
+        <RouterProvider router={router} />
       </main>
     </div>
   );
