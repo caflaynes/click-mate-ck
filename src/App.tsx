5818c18ad03417aa8f56708ec2b9454916f6b367
Carlo-Laynes <132353416+Carlo-Laynes@users.noreply.github.com>
2024-11-17 20:26:51 +0800

    imported hello.svg in App.scss

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Slogan from './components/Layouts/Slogan/Slogan';
 import Navbar from './components/Layouts/Navbar/Navbar';
 import Hero from './components/Sections/Hero/Hero';
 import logo from './assets/images/logo.svg';
+import hello from './assets/images/hero/hello.svg';
 
 const App: React.FC = () => {
   return (
@@ -14,7 +15,7 @@ const App: React.FC = () => {
       </header>
       <Navbar logoSrc={logo}/>
       <main>
-        <Hero helloUrl="./assets/images/hero/hello.svg" />
+        <Hero helloUrl={hello} />
       </main>
     </div>
   );
