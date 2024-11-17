2b20a29352f19cd98770abbb1f4d5b3884be8cb5
Carlo-Laynes <132353416+Carlo-Laynes@users.noreply.github.com>
2024-11-17 23:23:40 +0800

    tried fixing navlinks

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,6 @@ import './App.scss';
 import Slogan from './components/Layouts/Slogan/Slogan';
 import Navbar from './components/Layouts/Navbar/Navbar';
 
-// import HomePage from './pages/Home/HomePage';
-// import DigitalCamerasPage from './pages/Digital Cameras/DigitalCamerasPage';
-// import HowItWorksPage from './pages/How It Works/HowItWorksPage';
-// import PricingPage from './pages/Pricing/PricingPage';
-// import ContactUsPage from './pages/Contact Us/ContactUsPage';
-
-
-
 const App: React.FC = () => {
   return (
     <div className="App">
@@ -20,13 +12,6 @@ const App: React.FC = () => {
       </header>
       <Navbar logoSrc="./assets/images/logo.svg"/>
       <main>
-        {/* <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/digital-cameras" element={<DigitalCamerasPage />} />
-          <Route path="/how-it-works" element={<HowItWorksPage />} />
-          <Route path="/pricing" element={<PricingPage />} />
-          <Route path="/contact-us" element={<ContactUsPage />} />
-        </Routes> */}
       </main>
     </div>
   );
